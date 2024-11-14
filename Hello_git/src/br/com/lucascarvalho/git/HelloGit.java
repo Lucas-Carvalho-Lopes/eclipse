61db1e6147f543fb6a8a0aa31ace93d8f40d66db
Lucas-Carvalho-Lopes <lucas.carvalho.lopes016@gmail.com>
2024-11-14 16:21:55 -0300

    Adicionei arquivos via upload

---
+++ Hello_git/src/br/com/lucascarvalho/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.lucascarvalho.git;
+
+/**
+ * Exemplo de integração do Eclipse com o github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello git");
+	}
+
+}
